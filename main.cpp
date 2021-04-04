560c236ac2050aff11b68bb82cf054389f957141
Bob (ZhiltsovAI) <mpei.mpei.ru>
2021-04-04 20:21:28 +0300

    code: добавлен вывод минимума

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@ int main()
     << "A - B = " << a - b << '\n'
     << "A * B = " << a * b << '\n'
     << "A / B = " << a / b << '\n';
+
     if (a>=b) cout << "max = "<< a;
     else cout << " max = " << b;
+
+    if (a <= b) cout << "min = "<< a;
+    else cout << "min =" << b;
+
 }
